083cb1956257a3fa6ba5bb52a8b0e38feeafd9cb
ELLIS WOODS <ew280189@falmouth.ac.uk>
2024-08-23 13:52:03 +0100

    Fixed Player Controller

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -10,121 +10,90 @@ public interface Iinteractable
 
 public class PlayerController : MonoBehaviour
 {
-    // Player Controls
-
     private bool acceptingInputs;
-
-    // Player Components
+    private CharacterController cc;
     private Camera cam;
-    private Rigidbody rb;
+    Vector2 moveInput;
+    Vector2 lookXY;
+    public float horizontalMouse;
+    public float verticalMouse;
+    private Vector3 velocity;
+    public Transform groundCheck;
+    public float groundDistance;
+    public LayerMask groundLayerMask;
+    bool isGrounded;
 
-    // Player Movement
-    private Vector2 moveDirection;
-    [SerializeField] private float moveSpeed;
-    [SerializeField] private float xSens;
-    [SerializeField] private float ySens;
-    private Vector2 lookDelta;
-    private Vector2 lookXY;
-
-    // Player Jumping
-    private bool isOnGround;
-    [SerializeField] private float jumpVelocity;
-    [SerializeField] private float gravity;
-
+    public float speed;
+    Vector3 moveDirection
+    {
+        get
+        {
+            return (transform.forward * moveInput.y + transform.right * moveInput.x).normalized;
+        }
+    }
     private void Awake()
     {
-        cam = Camera.main;
-        lookXY = new Vector2(cam.transform.eulerAngles.x, transform.eulerAngles.y);
+        Physics.gravity *= 2;
+        cc = GetComponent<CharacterController>();
     }
-
     private void Start()
     {
-        Initialise();
         Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Locked;
-        //Physics.gravity = new Vector3(0, -gravity, 0);
-
+        cam = Camera.main;
+        groundCheck = transform.Find("GroundCheckLocation");
     }
-
-    private void Initialise()
-    {
-
-        rb = GetComponent<Rigidbody>();
-
-    }
-    bool downwardsPressure = true;
     private void Update()
     {
-        AssignConstantVariables();
-        LookUpdate();
-        InputCheck();
-        MovePlayer();
-        //if (rb.velocity.y < 0.1f && rb.velocity.y > -0.1f) isOnGround = true;
-        if (downwardsPressure)
+        AssignVariables();
+        if (!acceptingInputs)
         {
-            rb.AddForce(Vector3.down * 100);
+            Move();
+            Rotate();
+            CheckInputs();
         }
     }
 
-    private Vector2 oldLookPos = Vector2.zero;
-    private void AssignConstantVariables()
+    private void CheckInputs()
     {
-        //lookDelta = (Vector2)Input.mousePosition - oldLookPos;
-        lookDelta = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
-        moveDirection = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
-        //oldLookPos = Input.mousePosition;
+        if (Input.GetKeyDown(KeyCode.Space)) Jump();
     }
-
-    private void FixedUpdate()
+    private void Jump()
     {
-
-    }
-
-    private void MovePlayer()
-    {
-        Vector3 direction = transform.forward * moveDirection.y + transform.right * moveDirection.x;
-        rb.velocity = new Vector3(direction.normalized.x * moveSpeed * Time.deltaTime, rb.velocity.y, direction.normalized.z * moveSpeed * Time.deltaTime);
-        //rb.AddForce(direction.normalized * moveSpeed, ForceMode.Force);
-    }
-
-    private void LookUpdate()
-    {
-        lookXY.x -= lookDelta.y * xSens * Time.smoothDeltaTime;
-        lookXY.y += lookDelta.x * ySens * Time.smoothDeltaTime;
-        lookXY.x = Mathf.Clamp(lookXY.x, -90f, 90f);
-        transform.rotation = Quaternion.Euler(0, lookXY.y, 0);
-        cam.transform.rotation = Quaternion.Euler(lookXY.x, lookXY.y, 0);
-    }
-
-    private void OnCollisionEnter(Collision collision)
-    {
-        isOnGround = true;
-    }
-
-    private void OnCollisionExit(Collision collision)
-    {
-        isOnGround = false;
-    }
-    private void InputCheck()
-    {
-        if (Input.GetKeyDown(KeyCode.Space)) OnJump();
-        if(Input.GetKeyUp(KeyCode.Space)) ReleaseJump();
-        if (Input.GetKeyDown(KeyCode.E)) Interact();
-    }
-
-    void ReleaseJump()
-    {
-        downwardsPressure = true;
-    }
-
-    private void OnJump()
-    {
-        print(isOnGround);
-        if (isOnGround)
+        if (isGrounded)
         {
-            downwardsPressure = false;
-            rb.AddForce(0, jumpVelocity, 0);
+            velocity.y = 10f;
         }
+    }
+    private void Move()
+    {
+        cc.Move(moveDirection * speed* Time.deltaTime);
+        if (isGrounded && velocity.y < 0) velocity = Physics.gravity.normalized * 2;
+        else
+        {
+            velocity += Physics.gravity * Time.deltaTime;
+            cc.Move(velocity * Time.deltaTime);
+        }
+    }
+    private void Rotate()
+    {
+        transform.eulerAngles = new Vector3(0, lookXY.x, 0);
+        cam.transform.eulerAngles = new Vector3(lookXY.y, lookXY.x, 0);
+    }
+
+    private void AssignVariables()
+    {
+        Vector2 look;
+        look.x = Input.GetAxis("Mouse X")*horizontalMouse*Time.smoothDeltaTime;
+        look.y = Input.GetAxis("Mouse Y")*-verticalMouse*Time.smoothDeltaTime;
+
+        lookXY += look;
+        lookXY.y = Mathf.Clamp(lookXY.y, -90, 90);
+
+        moveInput.x = Input.GetAxis("Horizontal");
+        moveInput.y = Input.GetAxis("Vertical");
+
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundLayerMask);
     }
     private void Interact()
     {
